20adfde2dbccc924f3a8db4a3105a1dd723cc3ba
Stepan <kvantoreps@gmail.com>
2025-05-06 11:04:19 +0300

    feat: add roboto font import

--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,13 @@
-import {defineConfig, presetWind4} from 'unocss'
+import { defineConfig, presetWind4 } from 'unocss';
 
 export default defineConfig({
-    presets: [
-        presetWind4(),
-    ],
-})
+  presets: [
+    presetWind4(),
+    presetWebFonts({
+      provider: 'google',
+      fonts: {
+        Roboto: 'Roboto:300,500,600,800',
+      },
+    }),
+  ],
+});
